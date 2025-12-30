538ffcd4112e1e1ec8414cb27ab84466b3558aa3
Enhe Bai <enhe2006@gmail.com>
2025-12-30 15:19:39 -0500

    classroom and ui

---
+++ Assets/_Scripts/SafeArea.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeArea : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Rect lastSafeArea = new Rect(0, 0, 0, 0);
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    void Update()
+    {
+        // Re-apply if orientation or resolution changes
+        if (Screen.safeArea != lastSafeArea)
+        {
+            ApplySafeArea();
+        }
+    }
+
+    void ApplySafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+        lastSafeArea = safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+    }
+}
